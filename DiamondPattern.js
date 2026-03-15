d557ae81ac4cca14a05b410ffdcb1cc1809b8016
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-15 14:52:27 +0530

    Update DiamondPattern.js

--- DiamondPattern.js
+++ DiamondPattern.js
@@ -1,13 +1,22 @@
-const DIAMOND_SIZE = 4;
+const DIAMOND_SIZE = 5;
 
-for (let i = -DIAMOND_SIZE; i <= DIAMOND_SIZE; i++) {
-    let row = "";
-    for (let j = -DIAMOND_SIZE; j <= DIAMOND_SIZE; j++) {
-        if (Math.abs(i) + Math.abs(j) === DIAMOND_SIZE) {
-            row += "*";
-        } else {
-            row += " ";
-        }
+for (let i = 1; i <= 2 * DIAMOND_SIZE - 1; i++) {
+
+    let row = i <= DIAMOND_SIZE ? i : 2 * DIAMOND_SIZE - i;
+    let line = "";
+
+    for (let j = row; j < DIAMOND_SIZE; j++) {
+        line += " ";
     }
-    console.log(row);
+
+    line += "*";
+
+    if (row > 1) {
+        for (let j = 1; j <= 2 * row - 3; j++) {
+            line += " ";
+        }
+        line += "*";
+    }
+
+    console.log(line);
 }
